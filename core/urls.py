04a7f129da6b1f6320e71118238224b7824b37e3
Omer Cesur <cesuromersenyurt@hotmail.com>
2026-05-21 16:51:11 +0300

    Dosya paylasimi

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from files.views import RegisterView, FileListCreateView, FileDetailView
+from files.views import RegisterView, FileListCreateView, FileDetailView, FileShareView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # Kimlik İşlemleri
     path('api/register/', RegisterView.as_view(), name='register'),
     path('api/login/', TokenObtainPairView.as_view(), name='login'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    # Dosya İşlemleri (İşte o 4 Endpoint)
     path('api/files/', FileListCreateView.as_view(), name='file-list-create'),
     path('api/files/<int:pk>/', FileDetailView.as_view(), name='file-detail'),
+
+    # YENİ ENDPOINT
+    path('api/files/<int:pk>/share/', FileShareView.as_view(), name='file-share'),
 ]
--- files/models.py
+++ files/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class CloudFile(models.Model):
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_files')
 
 
     file_name = models.CharField(max_length=255)
@@ -15,6 +15,8 @@ class CloudFile(models.Model):
 
     upload_date = models.DateTimeField(auto_now_add=True)
     last_download_date = models.DateTimeField(null=True, blank=True)
+
+    shared_with = models.ManyToManyField(User, related_name='shared_files', blank=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.file_name}"
